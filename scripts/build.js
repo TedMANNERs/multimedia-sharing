a1dcea81c7f6706124de69b0272689af8999eab3
TedMANNERs <mannersofted@gmail.com>
2017-11-19 23:04:45 +0100

    Might this make Heroku work?

--- scripts/build.js
+++ scripts/build.js
@@ -11,178 +11,172 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-const compile = require("./webpackCompile");
-const cesiumConfig = require('../config/webpack.cesium.dll.config.js');
-compile("cesium", cesiumConfig)
-.then( ({stats}) => {
-  
-  // Ensure environment variables are read.
-  require('../config/env');
+// Ensure environment variables are read.
+require('../config/env');
 
-  const glob = require("glob")
-  const path = require('path');
-  const chalk = require('chalk');
-  const fs = require('fs-extra');
-  const webpack = require('webpack');
-  const config = require('../config/webpack.config.prod');
-  const paths = require('../config/paths');
-  const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
-  const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-  const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
-  const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
-  const printBuildError = require('react-dev-utils/printBuildError');
+const glob = require("glob")
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.prod');
+const paths = require('../config/paths');
+const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
+const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
+const printBuildError = require('react-dev-utils/printBuildError');
 
-  const measureFileSizesBeforeBuild =
-    FileSizeReporter.measureFileSizesBeforeBuild;
-  const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
-  const useYarn = fs.existsSync(paths.yarnLockFile);
+const measureFileSizesBeforeBuild =
+  FileSizeReporter.measureFileSizesBeforeBuild;
+const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
+const useYarn = fs.existsSync(paths.yarnLockFile);
 
-  // These sizes are pretty large. We'll warn for bundles exceeding them.
-  const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
-  const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
+// These sizes are pretty large. We'll warn for bundles exceeding them.
+const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
+const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 
-  // Warn and crash if required files are missing
-  if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
-    process.exit(1);
-  }
+// Warn and crash if required files are missing
+if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
+  process.exit(1);
+}
 
-  // First, read the current file sizes in build directory.
-  // This lets us display how much they changed later.
-  measureFileSizesBeforeBuild(paths.appBuild)
-    .then(previousFileSizes => {
-      // Remove all content but keep the directory so that
-      // if you're in it, you don't end up in Trash
-      fs.emptyDirSync(paths.appBuild);
-      // Merge with the public folder
-      copyPublicFolder();
-      copyCesium();
-      // Start the webpack build
-      return build(previousFileSizes);
-    })
-    .then(
-      ({ stats, previousFileSizes, warnings }) => {
-        if (warnings.length) {
-          console.log(chalk.yellow('Compiled with warnings.\n'));
-          console.log(warnings.join('\n\n'));
-          console.log(
-            '\nSearch for the ' +
-              chalk.underline(chalk.yellow('keywords')) +
-              ' to learn more about each warning.'
-          );
-          console.log(
-            'To ignore, add ' +
-              chalk.cyan('// eslint-disable-next-line') +
-              ' to the line before.\n'
-          );
-        } else {
-          console.log(chalk.green('Compiled successfully.\n'));
-        }
-
-        console.log('File sizes after gzip:\n');
-        printFileSizesAfterBuild(
-          stats,
-          previousFileSizes,
-          paths.appBuild,
-          WARN_AFTER_BUNDLE_GZIP_SIZE,
-          WARN_AFTER_CHUNK_GZIP_SIZE
+// First, read the current file sizes in build directory.
+// This lets us display how much they changed later.
+measureFileSizesBeforeBuild(paths.appBuild)
+  .then(previousFileSizes => {
+    // Remove all content but keep the directory so that
+    // if you're in it, you don't end up in Trash
+    fs.emptyDirSync(paths.appBuild);
+    // Merge with the public folder
+    copyPublicFolder();
+    copyCesium();
+    // Start the webpack build
+    return build(previousFileSizes);
+  })
+  .then(
+    ({ stats, previousFileSizes, warnings }) => {
+      if (warnings.length) {
+        console.log(chalk.yellow('Compiled with warnings.\n'));
+        console.log(warnings.join('\n\n'));
+        console.log(
+          '\nSearch for the ' +
+            chalk.underline(chalk.yellow('keywords')) +
+            ' to learn more about each warning.'
         );
-        console.log();
-
-        const appPackage = require(paths.appPackageJson);
-        const publicUrl = paths.publicUrl;
-        const publicPath = config.output.publicPath;
-        const buildFolder = path.relative(process.cwd(), paths.appBuild);
-        printHostingInstructions(
-          appPackage,
-          publicUrl,
-          publicPath,
-          buildFolder,
-          useYarn
+        console.log(
+          'To ignore, add ' +
+            chalk.cyan('// eslint-disable-next-line') +
+            ' to the line before.\n'
         );
-      },
-      err => {
-        console.log(chalk.red('Failed to compile.\n'));
-        printBuildError(err);
-        process.exit(1);
+      } else {
+        console.log(chalk.green('Compiled successfully.\n'));
       }
-    );
 
-  // Create the production build and print the deployment instructions.
-  function build(previousFileSizes) {
-    console.log('Creating an optimized production build...');
+      console.log('File sizes after gzip:\n');
+      printFileSizesAfterBuild(
+        stats,
+        previousFileSizes,
+        paths.appBuild,
+        WARN_AFTER_BUNDLE_GZIP_SIZE,
+        WARN_AFTER_CHUNK_GZIP_SIZE
+      );
+      console.log();
 
-    let compiler = webpack(config);
-    return new Promise((resolve, reject) => {
-      compiler.run((err, stats) => {
-        if (err) {
-          return reject(err);
+      const appPackage = require(paths.appPackageJson);
+      const publicUrl = paths.publicUrl;
+      const publicPath = config.output.publicPath;
+      const buildFolder = path.relative(process.cwd(), paths.appBuild);
+      printHostingInstructions(
+        appPackage,
+        publicUrl,
+        publicPath,
+        buildFolder,
+        useYarn
+      );
+    },
+    err => {
+      console.log(chalk.red('Failed to compile.\n'));
+      printBuildError(err);
+      process.exit(1);
+    }
+  );
+
+// Create the production build and print the deployment instructions.
+function build(previousFileSizes) {
+  console.log('Creating an optimized production build...');
+
+  let compiler = webpack(config);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+      const messages = formatWebpackMessages(stats.toJson({}, true));
+      if (messages.errors.length) {
+        // Only keep the first error. Others are often indicative
+        // of the same problem, but confuse the reader with noise.
+        if (messages.errors.length > 1) {
+          messages.errors.length = 1;
         }
-        const messages = formatWebpackMessages(stats.toJson({}, true));
-        if (messages.errors.length) {
-          // Only keep the first error. Others are often indicative
-          // of the same problem, but confuse the reader with noise.
-          if (messages.errors.length > 1) {
-            messages.errors.length = 1;
-          }
-          return reject(new Error(messages.errors.join('\n\n')));
-        }
-        if (
-          process.env.CI &&
-          (typeof process.env.CI !== 'string' ||
-            process.env.CI.toLowerCase() !== 'false') &&
-          messages.warnings.length
-        ) {
-          console.log(
-            chalk.yellow(
-              '\nTreating warnings as errors because process.env.CI = true.\n' +
-                'Most CI servers set it automatically.\n'
-            )
-          );
-          return reject(new Error(messages.warnings.join('\n\n')));
-        }
-        return resolve({
-          stats,
-          previousFileSizes,
-          warnings: messages.warnings,
-        });
+        return reject(new Error(messages.errors.join('\n\n')));
+      }
+      if (
+        process.env.CI &&
+        (typeof process.env.CI !== 'string' ||
+          process.env.CI.toLowerCase() !== 'false') &&
+        messages.warnings.length
+      ) {
+        console.log(
+          chalk.yellow(
+            '\nTreating warnings as errors because process.env.CI = true.\n' +
+              'Most CI servers set it automatically.\n'
+          )
+        );
+        return reject(new Error(messages.warnings.join('\n\n')));
+      }
+      return resolve({
+        stats,
+        previousFileSizes,
+        warnings: messages.warnings,
       });
     });
-  }
+  });
+}
 
-  function copyPublicFolder() {
-    fs.copySync(paths.appPublic, paths.appBuild, {
-      dereference: true,
-      filter: file => file !== paths.appHtml,
-    });
-  }
+function copyPublicFolder() {
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: file => file !== paths.appHtml,
+  });
+}
 
-  function copyCesium() {
-    const outputPath = path.join(paths.appBuild, "cesium");
-    const inputPath = path.cesiumProdBuild + "/**/*.js";
+function copyCesium() {
+  const outputPath = path.join(paths.appBuild, "cesium");
+  const inputPath = path.cesiumProdBuild + "/**/*.js";
 
-    let globOptions = {
-        nodir : true,
-        cwd : "node_modules/cesium/Build/Cesium/",
-        ignore: ["*Cesium.js", "**/NaturalEarthII/**/*", "**/maki/**/*"]
-    };
+  let globOptions = {
+      nodir : true,
+      cwd : "node_modules/cesium/Build/Cesium/",
+      ignore: ["*Cesium.js", "**/NaturalEarthII/**/*", "**/maki/**/*"]
+  };
 
-    glob("**/*", globOptions, function (er, files) {
-        // files is an array of filenames.
-        // If the `nonull` option is set, and nothing
-        // was found, then files is ["**/*.js"]
-        // er is an error object or null.
+  glob("**/*", globOptions, function (er, files) {
+      // files is an array of filenames.
+      // If the `nonull` option is set, and nothing
+      // was found, then files is ["**/*.js"]
+      // er is an error object or null.
 
-        files.forEach(function (srcPath) {
-            let fullSrcPath = path.join("node_modules/cesium/Build/Cesium/", srcPath);
-            let fullDestPath = path.join(outputPath, srcPath);
-            fs.copySync(fullSrcPath, fullDestPath);
-        });
-    });
+      files.forEach(function (srcPath) {
+          let fullSrcPath = path.join("node_modules/cesium/Build/Cesium/", srcPath);
+          let fullDestPath = path.join(outputPath, srcPath);
+          fs.copySync(fullSrcPath, fullDestPath);
+      });
+  });
 
-    const cesiumDllPath = path.join(paths.app, "distdll/cesiumDll.js");
-    const cesiumOutputPath = path.join(outputPath, "cesiumDll.js");
-    fs.copySync(cesiumDllPath, cesiumOutputPath);
+  const cesiumDllPath = path.join(paths.app, "distdll/cesiumDll.js");
+  const cesiumOutputPath = path.join(outputPath, "cesiumDll.js");
+  fs.copySync(cesiumDllPath, cesiumOutputPath);
 
-    console.log("Cesium copied to output folder");
-  }
-});
+  console.log("Cesium copied to output folder");
+}
