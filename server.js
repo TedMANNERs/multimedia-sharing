40dbdf7721b361ecdc8c8dfe0ad05cdab614e0ed
TedMANNERs <mannersofted@gmail.com>
2017-12-10 19:21:22 +0100

    Fix port

--- server.js
+++ server.js
@@ -163,7 +163,7 @@
         res.send(apiKey)
     });
 
-    var server = app.listen(argv.port, argv.public ? undefined : 'localhost', function() {
+    var server = app.listen(process.env.PORT || 8080, function() {
         if (argv.public) {
             console.log('Cesium development server running publicly.  Connect to http://localhost:%d/', server.address().port);
         } else {
